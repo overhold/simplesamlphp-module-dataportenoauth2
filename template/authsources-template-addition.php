97a48caa60738343c8d68df2f22d71a64553d002
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-07-19 16:05:20 +0200

    Added templates

---
+++ template/authsources-template-addition.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+  'dataporten-oauth2' => array(
+      'dataportenoauth2:Connect',
+      'client_id'           => '*****-*****-*****-*****-*****',
+      'client_secret'       => '*****-*****-*****-*****-*****',
+  ),
+
+);
